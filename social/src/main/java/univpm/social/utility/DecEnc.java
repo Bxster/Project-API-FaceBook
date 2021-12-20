bc94ee00c5d5cf75b74bc41909bca05548bb16d7
lucabellantee <s1097955@studenti.univpm.it>
2021-12-20 13:01:03 +0100

    Completata la classe DecEnc

--- social/src/main/java/univpm/social/utility/DecEnc.java
+++ social/src/main/java/univpm/social/utility/DecEnc.java
@@ -1,11 +1,14 @@
 package univpm.social.utility;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import univpm.social.exceptions.FileException;
 import univpm.social.model.Albums;
+import univpm.social.model.Photo;
 import univpm.social.model.User;
 
 
@@ -51,6 +54,47 @@ public static JSONObject encodeToJson(User user)
 	
 	System.out.println(toGive.toJSONString());
 	return toGive; 
+	
+	
+   }
+
+
+//TODO LUCA : RICHIAMARE ECCEZIONE CHE CONSENTE DI CAPIRE SE UN PROFILO
+//NON HA ALBUM E CHE NE STAMPI UN ECCEZIONE
+
+   public static User decodeToUser() throws IOException, FileException 
+     {
+       ServiceImpl service = new ServiceImpl();
+       JSONObject jsonobject = service.getGeneralInformation();
+       User user;
+       ArrayList<Albums> albums = new ArrayList() ;
+       ArrayList<Photo> photo = new ArrayList();
+
+
+
+      //ORA ACCEDO AL ALBUMS E PRENDO IL JSONARRAY : "data"
+      JSONObject Albums =  (JSONObject) jsonobject.get("albums");
+      JSONArray data = (JSONArray) Albums.get("data");
+
+
+      for(int i = 0 ; i<data.size() ; i++) 
+        {
+           Object obj = data.get(i);
+           JSONObject jobj = (JSONObject) obj; 
+
+           String albumName = jobj.get("name").toString();
+           String createdTime = jobj.get("created_time").toString();
+           String albumId = jobj.get("id").toString();
+           Albums album = new Albums(albumName,createdTime,albumId,null);	
+           albums.add(album);
+        }
+
+
+       user = new User(jsonobject.get("name").toString() , jsonobject.get("id").toString() , jsonobject.get("email").toString() ,jsonobject.get("birthday").toString(), albums);
+
+       
+      return user;
 }
+
 
 }
