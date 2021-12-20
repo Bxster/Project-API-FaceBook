97e6ca977af3ee0c459da74413be8c7e1b891801
lucabellantee <s1097955@studenti.univpm.it>
2021-12-20 13:21:52 +0100

    metodo per verificare se l'anno è ammissibile oppure no

--- social/src/main/java/univpm/social/errorscheck/ErrorsCheck.java
+++ social/src/main/java/univpm/social/errorscheck/ErrorsCheck.java
@@ -1,9 +1,13 @@
 package univpm.social.errorscheck;
 
+import java.time.LocalDate;
+
 import univpm.social.exceptions.BadParameterException;
 
 public class ErrorsCheck {
 	
+	
+	private final static  String commonMessage = "VERIFICA LA PRESENZA DI ALTRI ERRORI NEL CODICE ";
 	
 	private static boolean isNumber(String day , String month , String year) throws BadParameterException 
 	{
@@ -19,6 +23,19 @@ public class ErrorsCheck {
 			throw new NumberFormatException("PUOI INSERIRE SOLO NUMERI");
 	    }
 	return false;
+	}
+	
+	
+	private static boolean correctYear(String number) throws BadParameterException 
+	{
+		LocalDate now = LocalDate.now();
+		
+		if(Integer.parseInt(number) <= now.getYear()  && Integer.parseInt(number)>= 2005) 
+			return true;
+	
+		  throw new BadParameterException("ANNO NON VALIDO ! " +   commonMessage);
+		                              
+                               
 	}
 
 }
