bbed1ab8fec67d670e6cbf808f01b22773ecc411
lucabellantee <s1097955@studenti.univpm.it>
2021-12-20 15:23:00 +0100

    Chiamata del metodo statico della Classe PortScanning

--- social/src/main/java/univpm/social/FacebookProjectApplication.java
+++ social/src/main/java/univpm/social/FacebookProjectApplication.java
@@ -1,13 +1,34 @@
 package univpm.social;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import univpm.social.portscanning.PortScanning;
+
+
 
 @SpringBootApplication
+@ComponentScan({"univpm.social.service","univpm.social.controller","univpm.social.exceptions"})
 public class FacebookProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FacebookProjectApplication.class, args);
+		
+		
+		/*
+		    Richiama il metodo statico della classe Final PortScanning
+		    successivamente prende la porta libera e avvia l'applicazione
+		    springboot.
+    	*/
+		
+		String port = Integer.toString(PortScanning.getPort().getLocalPort());
+		
+		SpringApplication app = new SpringApplication(FacebookProjectApplication.class);
+	        app.setDefaultProperties(Collections
+	          .singletonMap("server.port", port));
+	        app.run(args);
 	}
 
 }
