10647685f120ac337405ac95b6cce48a717185a6
lucabellantee <s1097955@studenti.univpm.it>
2021-12-20 12:35:09 +0100

    Creazione di un altra classe di eccezioni per profili senza albums

---
+++ social/src/main/java/univpm/social/exceptions/NoAlbumsException.java
@@ -0,0 +1,18 @@
+package univpm.social.exceptions;
+
+public class NoAlbumsException extends Exception{
+	
+	
+	public NoAlbumsException() 
+	{
+		super();
+	}
+	
+	
+	public NoAlbumsException(String msg) 
+	{
+		super(msg);
+	}
+
+
+}
