ef33ef51923f5b53cfc9f0c7588c8f375631a179
lucabellantee <s1097955@studenti.univpm.it>
2021-12-20 16:17:47 +0100

    Fine creazione della classe Statistic + aggiunto metodo per la ricerca di album con nomi volgari

--- social/src/main/java/univpm/social/statistics/Statistic.java
+++ social/src/main/java/univpm/social/statistics/Statistic.java
@@ -6,10 +6,13 @@ import java.text.ParseException;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+
+
 import univpm.social.exceptions.BadParameterException;
 import univpm.social.exceptions.FileException;
 import univpm.social.exceptions.NoAlbumsException;
 import univpm.social.filters.FilterImpl;
+import univpm.social.filters.FilterName;
 
 
 
@@ -34,6 +37,24 @@ public class Statistic {
 	    objFiltered.put("statistica per il filtro corrente", toPut);
 			
 	     return objFiltered;
+	}
+	
+	
+	public  JSONObject getNameStatistic(String name) throws IOException, FileException 
+	{
+		
+		filter = new FilterName();
+		
+		JSONObject jsonFiltered = ((FilterName) filter).filterForName(name);
+		
+		if(jsonFiltered.get("ERROR") != null) return jsonFiltered;
+		
+	    JSONArray arrayFiltered = (JSONArray) jsonFiltered.get("albums");
+		
+	    String toPut = Integer.toString(arrayFiltered.size()) + "Album catturato/i dal filtro name: " + name;
+	    jsonFiltered.put("statistica per il filtro corrente", toPut);
+			
+	     return jsonFiltered;
 	}
 	
 
