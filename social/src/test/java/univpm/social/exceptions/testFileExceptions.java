480f4afe57c1f5ba3446a3ce40da01260038b404
lucabellantee <s1097955@studenti.univpm.it>
2021-12-20 19:50:18 +0100

    Creazione classe testFileExceptions

---
+++ social/src/test/java/univpm/social/exceptions/testFileExceptions.java
@@ -0,0 +1,24 @@
+package univpm.social.exceptions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class testFileExceptions {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
