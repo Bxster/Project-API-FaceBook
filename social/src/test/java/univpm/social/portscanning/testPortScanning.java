ca29dd104d9bfebb87894d06fcd36fd6d9c594da
lucabellantee <s1097955@studenti.univpm.it>
2021-12-21 10:54:03 +0100

    Implementato test Port scanning + revisione

--- social/src/test/java/univpm/social/portscanning/testPortScanning.java
+++ social/src/test/java/univpm/social/portscanning/testPortScanning.java
@@ -6,8 +6,11 @@ import java.net.ServerSocket;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
 
 class testPortScanning {
 
@@ -33,9 +36,14 @@ class testPortScanning {
 	void tearDown() throws Exception {
 	}
 
+
+
 	@Test
 	void test() {
-		fail("Not yet implemented");
+	
+		Assertions.assertEquals(8180, PortScanning.getPort().getLocalPort() );
+	 
 	}
+
 
 }
