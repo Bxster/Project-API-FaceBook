4873d92cbea86a08c288d75b86bc79e5b1d813c7
lucabellantee <s1097955@studenti.univpm.it>
2021-12-21 10:14:01 +0100

    Aggiunto test per verificare la gestione del eccezione dello user token(se non è scritto tra la seconda e la terza riga)

--- social/src/test/java/univpm/social/exceptions/testFileExceptions.java
+++ social/src/test/java/univpm/social/exceptions/testFileExceptions.java
@@ -2,23 +2,47 @@ package univpm.social.exceptions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import univpm.social.utility.FileExtern;
+
+
 class testFileExceptions {
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
+    
+	
+	/*
+	     Test nel caso del file WRITE_ME , NOTARE IL PARAMETRO TRUE 
+	     NEL .ReadFromFile(... , true)
+	 */
+	
+	// TODO LUCA : RIVEDERE QUESTA PARTE 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void  testUncorrectLine() throws IOException 
+	{
+		FileWriter file = new FileWriter("src\\test\\java\\com\\example\\demo\\WRITE_ME_TEST.txt");
+		PrintWriter printFile = new PrintWriter(file);
+		
+		printFile.println("01246789");
+		printFile.println("ashbahja5345");
+		printFile.println(" ");
+		printFile.println("dfanfkak456");
+		printFile.close();
+		
+		String path = "src\\test\\java\\com\\example\\demo\\WRITE_ME_TEST.txt";
+		FileException thrown = Assertions.assertThrows(FileException.class, () -> {
+			FileExtern.readFromFile(path,true);
+		});
+		
+		Assertions.assertEquals("SBAGLIATO ! DEVI INSERIRE IL TOKEN NELLA SECONDA E TERZA RIGA", thrown.getMessage());
 	}
+	
 
 }
