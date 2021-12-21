d8a69ad569ea2c695d75ca9d40773fdba2a33940
lucabellantee <s1097955@studenti.univpm.it>
2021-12-21 11:30:24 +0100

    Aggiunta del test per nome non presente negli albums

--- social/src/test/java/univpm/social/filter/testFilterText.java
+++ social/src/test/java/univpm/social/filter/testFilterText.java
@@ -2,16 +2,36 @@ package univpm.social.filter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
+import univpm.social.exceptions.FileException;
+import univpm.social.exceptions.NoAlbumsException;
+import univpm.social.filters.FilterName;
+
 class testFilterText {
 
-
+    
+	/*
+	     Testo che se passo un nome non presente negli albums , in questo caso "pasquale"
+	     il metodo della sottoclasse FilterName mi stampa un jsonobject di 
+	     avvertimento/errore
+	 */
+	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test() throws IOException, FileException {
+        
+		FilterName filter = new FilterName();
+		JSONObject jsonobject = new JSONObject();
+		jsonobject.put("ERROR", "NESSUN ALBUM TROVATO CON IL NOME pasquale");
+		Assertions.assertEquals(jsonobject,filter.filterForName("pasquale"));
 	}
 
 }
