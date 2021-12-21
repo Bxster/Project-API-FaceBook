c4bf9af1d6fa026ed4319bb322c63fd25b418b66
lucabellantee <s1097955@studenti.univpm.it>
2021-12-21 10:50:31 +0100

    Modificato contenuto file .txt + creazione file classe dt test per i filtri per nome

---
+++ social/src/test/java/univpm/social/filter/testFilterText.java
@@ -0,0 +1,17 @@
+package univpm.social.filter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class testFilterText {
+
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
