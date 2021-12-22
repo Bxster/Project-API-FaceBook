2ee2911b306d3cee92b568f3d2f8f8cee32e42f1
Bxster <baldellimarketing@gmail.com>
2021-12-22 15:21:26 +0100

    Ricaricamento javadoc per le classi precedenti

--- social/doc/package-search-index.js
+++ social/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"univpm.social.errorscheck"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"univpm.social.filters"}];updateSearchResults();
